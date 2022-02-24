71cd46c7f2a4feff3c9f74348b464260059d1934
saiful Islam <saifulmeraz897@gmail.com>
2022-02-24 22:20:22 +0600

    adding signup form

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -6,45 +6,43 @@
         <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
           <div class="card-body p-4 p-md-5">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
-            <form>
-
+            <form @submit.prevent="userRegistration">
               <div class="row">
                 <div class="col-md-6 mb-4">
-
                   <div class="form-outline">
-                    <input type="text" id="firstName" class="form-control form-control-lg" />
-                    <label class="form-label" for="firstName">First Name</label>
+					  <label class="form-label" for="firstName">First Name</label>
+                    <input type="text" id="firstName" 
+					class="form-control form-control-lg" 
+					v-model="userInfo.fName"
+					/>
                   </div>
-
                 </div>
                 <div class="col-md-6 mb-4">
-
+					<label class="form-label" for="lastName">Last Name</label>
                   <div class="form-outline">
-                    <input type="text" id="lastName" class="form-control form-control-lg" />
-                    <label class="form-label" for="lastName">Last Name</label>
+                    <input type="text" id="lastName" 
+					class="form-control form-control-lg" 
+					v-model="userInfo.lName"
+					/>
                   </div>
-
                 </div>
               </div>
-
               <div class="row">
                 <div class="col-md-6 mb-4 d-flex align-items-center">
-
                   <div class="form-outline datepicker w-100">
+					  <label for="birthdayDate" class="form-label">Birthday</label>
                     <input
                       type="text"
                       class="form-control form-control-lg"
                       id="birthdayDate"
+					  v-model="userInfo.bDate"
                     />
-                    <label for="birthdayDate" class="form-label">Birthday</label>
                   </div>
-
                 </div>
                 <div class="col-md-6 mb-4">
-
                   <h6 class="mb-2 pb-1">Gender: </h6>
-
                   <div class="form-check form-check-inline">
+					  <label class="form-check-label" for="femaleGender">Female</label>
                     <input
                       class="form-check-input"
                       type="radio"
@@ -52,57 +50,46 @@
                       id="femaleGender"
                       value="option1"
                       checked
+					  v-model="userInfo.gender"
                     />
-                    <label class="form-check-label" for="femaleGender">Female</label>
                   </div>
 
                   <div class="form-check form-check-inline">
+					  <label class="form-check-label" for="maleGender">Male</label>
                     <input
                       class="form-check-input"
                       type="radio"
                       name="inlineRadioOptions"
                       id="maleGender"
                       value="option2"
+					  v-model="userInfo.gender"
                     />
-                    <label class="form-check-label" for="maleGender">Male</label>
                   </div>
-
-                  <div class="form-check form-check-inline">
-                    <input
-                      class="form-check-input"
-                      type="radio"
-                      name="inlineRadioOptions"
-                      id="otherGender"
-                      value="option3"
-                    />
-                    <label class="form-check-label" for="otherGender">Other</label>
-                  </div>
-
                 </div>
               </div>
-
               <div class="row">
                 <div class="col-md-6 mb-4 pb-2">
-
                   <div class="form-outline">
-                    <input type="email" id="emailAddress" class="form-control form-control-lg" />
-                    <label class="form-label" for="emailAddress">Email</label>
+					  <label class="form-label" for="emailAddress">Email</label>
+                    <input type="email" id="emailAddress" 
+					class="form-control form-control-lg" 
+					v-model="userInfo.email"
+					/>
                   </div>
-
                 </div>
                 <div class="col-md-6 mb-4 pb-2">
-
-                  <div class="form-outline">
-                    <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
-                    <label class="form-label" for="phoneNumber">Phone Number</label>
-                  </div>
-
+					<label class="form-label" for="phoneNumber">Phone Number</label>
+					<div class="form-outline">
+						<input type="tel" id="phoneNumber" 
+						class="form-control form-control-lg" 
+						v-model="userInfo.pNumber"
+						/>
+					</div>
                 </div>
               </div>
               <div class="mt-4 pt-2">
                 <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
               </div>
-
             </form>
           </div>
         </div>
@@ -114,7 +101,23 @@
 
 <script>
 export default {
+	data(){
+		return{
+			userInfo:{
+				fName:"",
+				lName:"",
+				bDate:null,
+				gender:"",
+				email:"",
+				pNumber:""
+			}
+		}
+	},
+	methods:{
+		userRegistration(){
 
+		}
+	}
 }
 </script>
 
